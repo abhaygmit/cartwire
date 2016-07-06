9a9f9bb3b1fb5ba6a4f643e7927b4a06c68cd666
abhay mishra <abhay.mishra@srmtechsol.com>
2016-07-06 11:08:09 +0530

    test

--- mail_header.php
+++
@@ -1,8 +0,0 @@
-
-<table width="600" border="0" cellspacing="0" cellpadding="0" style="margin:auto; border:1px solid #000;">
-  <tr>
-    <td style="vertical-align:top;"><img src="<?php echo base_url();?>images/mail_images/cw-mailer-header-bg.jpg" alt="" /></td>
-  </tr>
- 
-   <tr>
-  <td style="padding:35px 40px; font-family:Helvetica; font-size:13px; line-height:18px; color:#252525; vertical-align:top; background:#e8e8e8;">
